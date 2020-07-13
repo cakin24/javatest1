17cf88363a760391de6b402ccdf6ba7bd503c106
chengqiuming <798103175@qq.com>
2020-07-13 22:15:51 +0800

    BeanUtils.copyProperties拷贝结构不同类测试

---
+++ src/main/java/invoice/InvoiceService.java
@@ -0,0 +1,15 @@
+package invoice;
+
+import invoice.dto.CertificationRequestDto;
+import invoice.dto.CertificationResponseDto;
+
+/**
+ * Copyright (C), 2020-2020, 软件公司
+ * FileName: huiduoduo
+ * Author:   cakin
+ * Date:     2020/7/13
+ * Description:
+ */
+public interface InvoiceService {
+    CertificationResponseDto certification(CertificationRequestDto dto);  // 认证接口
+}
