b759e2681dcdad4799d31dc3ecccdc7152ab1f0f
chengqiuming <798103175@qq.com>
2021-06-20 09:49:48 +0800

    项目整理

--- src/main/java/com/mashibing/juc/c_000/T01_WhatIsThread.java
+++ src/main/java/com/mashibing/juc/c_000/T01_WhatIsThread.java
@@ -6,21 +6,21 @@ public class T01_WhatIsThread {
     private static class T1 extends Thread {
         @Override
         public void run() {
-           for(int i=0; i<10; i++) {
-               try {
-                   TimeUnit.MICROSECONDS.sleep(1);
-               } catch (InterruptedException e) {
-                   e.printStackTrace();
-               }
-               System.out.println("T1");
-           }
+            for (int i = 0; i < 10; i++) {
+                try {
+                    TimeUnit.MICROSECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("T1");
+            }
         }
     }
 
     public static void main(String[] args) {
-        //new T1().run();
-        new T1().start();
-        for(int i=0; i<10; i++) {
+        new T1().run(); // 方法调用
+        //new T1().start(); // 启动一个新线程
+        for (int i = 0; i < 10; i++) {
             try {
                 TimeUnit.MICROSECONDS.sleep(1);
             } catch (InterruptedException e) {
@@ -28,6 +28,5 @@ public class T01_WhatIsThread {
             }
             System.out.println("main");
         }
-
     }
 }
--- src/main/java/com/mashibing/juc/c_000/T02_HowToCreateThread.java
+++ src/main/java/com/mashibing/juc/c_000/T02_HowToCreateThread.java
@@ -1,5 +1,6 @@
 package com.mashibing.juc.c_000;
 
+// 创建和启动线程
 public class T02_HowToCreateThread {
     static class MyThread extends Thread {
         @Override
@@ -16,13 +17,14 @@ public class T02_HowToCreateThread {
     }
 
     public static void main(String[] args) {
-        new MyThread().start();
-        new Thread(new MyRun()).start();
-        new Thread(()->{
+        new MyThread().start(); // 通过继承 Thread 创建和启动线程
+        new Thread(new MyRun()).start(); // 通过实现 Runnable 创建和启动线程
+        new Thread(() -> {
             System.out.println("Hello Lambda!");
-        }).start();
+        }).start(); // 通过 Lambda 表达式创建和启动线程，它实际是第2种的变形
     }
-
 }
-
-//请你告诉我启动线程的三种方式 1：Thread 2: Runnable 3:Executors.newCachedThrad
+// 启动线程的三种方式
+// 1：Thread
+// 2：Runnable
+// 3：Executors.newCachedThread 通过线程池启动线程
